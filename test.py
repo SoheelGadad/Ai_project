1ba5b4f8448da400d6fabaacd0600d1a94fb6298
Soheel Gadad <108049316+SoheelGadad@users.noreply.github.com>
2023-03-27 19:55:17 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,34 @@
+import tensorflow as tf
+from tensorflow.keras.datasets import cifar10
+
+# Load the CIFAR-10 dataset
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+
+# Normalize the pixel values between 0 and 1
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+# Define the model architecture
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(32, (3,3), activation='relu', input_shape=(32,32,3)),
+    tf.keras.layers.MaxPooling2D((2,2)),
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D((2,2)),
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(64, activation='relu'),
+    tf.keras.layers.Dense(10)
+])
+
+# Compile the model
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+# Train the model
+model.fit(x_train, y_train, epochs=10, validation_data=(x_test, y_test))
+
+# Evaluate the model
+model.evaluate(x_test, y_test)
+
+# Save the model
+model.save('my_model.h5')
